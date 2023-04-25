341b7da79c6ccd4ff65b4fe29af53e618029c091
Tadas <tadasmiloncius@gmail.com>
2023-04-25 16:43:54 +0700

    refurnished READY

--- components/Works.jsx
+++ components/Works.jsx
@@ -5,6 +5,10 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import kiss from '../public/kisss.png';
+import farm from '../public/FARMA.png';
+import logoDesign from '../public/logoDesign.png';
+import chefbox from '../public/chefbox.png';
+import beautyLogo from '../public/BEAUTYLOGO.png';
 import graphics from '../public/advert.png';
 import beauty from '../public/beauty.png';
 import personal from '../public/personal.png';
@@ -25,6 +29,54 @@ function Works() {
         <BsCamera2 className='w-14 h-auto m-auto' />
         <h1 className='text-center text-5xl tracking-widest'>PORTFOLIO</h1>
         <Slider {...settings} className='mt-5'>
+        <div className='px-3 group'>
+    <div className='relative cursor-pointer'>
+      <Image
+        src={logoDesign}
+        alt='logo'
+        className='w-80vw h-auto transition duration-500 ease-in-out group-hover:brightness-50'
+      />
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full bg-opacity-50 py-2 px-3 text-white transition duration-500 ease-in-out opacity-0 group-hover:opacity-100'>
+        <a href='https://www.youtube.com/watch?v=AZmjGi94pfI' className='text-xl tracking-widest'>LOGO ANIMATION</a>
+      </div>
+    </div>
+  </div>
+  <div className='px-3 group'>
+    <div className='relative cursor-pointer'>
+      <Image
+        src={chefbox}
+        alt='logo'
+        className='w-80vw h-auto transition duration-500 ease-in-out group-hover:brightness-50'
+      />
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full bg-opacity-50 py-2 px-3 text-white transition duration-500 ease-in-out opacity-0 group-hover:opacity-100'>
+        <a href='https://www.youtube.com/watch?v=sPKh2TFx_t0' className='text-xl tracking-widest'>EXPLAINER ANIMATION</a>
+      </div>
+    </div>
+  </div>
+  <div className='px-3 group'>
+    <div className='relative cursor-pointer'>
+      <Image
+        src={farm}
+        alt='logo'
+        className='w-80vw h-auto transition duration-500 ease-in-out group-hover:brightness-50'
+      />
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full bg-opacity-50 py-2 px-3 text-white transition duration-500 ease-in-out opacity-0 group-hover:opacity-100'>
+        <a href='https://www.youtube.com/watch?v=H6Iis4aYHdM' className='text-xl tracking-widest'>MOTION GRAPHICS</a>
+      </div>
+    </div>
+  </div>
+  <div className='px-3 group'>
+    <div className='relative cursor-pointer'>
+      <Image
+        src={beautyLogo}
+        alt='logo'
+        className='w-80vw h-auto transition duration-500 ease-in-out group-hover:brightness-50'
+      />
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full bg-opacity-50 py-2 px-3 text-white transition duration-500 ease-in-out opacity-0 group-hover:opacity-100'>
+        <a href='https://www.youtube.com/watch?v=kp0iWXPLmGs' className='text-xl tracking-widest'>PRODUCT ADVERTISEMENT</a>
+      </div>
+    </div>
+  </div>
   <div className='px-3 group'>
     <div className='relative cursor-pointer'>
       <Image
@@ -45,7 +97,7 @@ function Works() {
         className='w-80vw h-auto transition duration-500 ease-in-out group-hover:brightness-50'
       />
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full bg-opacity-50 py-2 px-3 text-white transition duration-500 ease-in-out opacity-0 group-hover:opacity-100'>
-        <a href='https://www.facebook.com/watch?v=731094172016573' className='text-xl tracking-widest'>MOTION GRAPHICS</a>
+        <a href='https://www.youtube.com/watch?v=uIlig0EcWP4' className='text-xl tracking-widest'>BRAND PROMO</a>
       </div>
     </div>
   </div>
@@ -73,6 +125,9 @@ function Works() {
       </div>
     </div>
   </div>
+  
+  
+  
 </Slider>
   </div>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='mt-[2rem]'><path fill="#c2613d" fill-opacity="1" d="M0,192L30,181.3C60,171,120,149,180,128C240,107,300,85,360,90.7C420,96,480,128,540,165.3C600,203,660,245,720,224C780,203,840,117,900,122.7C960,128,1020,224,1080,266.7C1140,309,1200,299,1260,261.3C1320,224,1380,160,1410,128L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
